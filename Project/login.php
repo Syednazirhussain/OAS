9777b46817b5bd21f48a4ebc560f0442d3f92248
Syed NAzir Hussain <syednazir13@gmail.com>
2017-10-03 22:08:38 -0700

    Modify login page

--- Project/login.php
+++ Project/login.php
@@ -25,7 +25,7 @@
     <!-- jQuery -->
     <script src="../vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
-
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- FastClick -->
     <script src="../vendors/fastclick/lib/fastclick.js"></script>
     <!-- NProgress -->
@@ -59,7 +59,7 @@
                 });
             });
             $('#submit').click(function () {
-                  /*alert('Jquerey working');*/
+                /* alert('working');*/
                 var text = '';
                 var jsObj = {};
                 var data = $('#Login').serializeArray();
