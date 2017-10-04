5d16b1e6e42f1ab56dd95bce9777e73e3d9ddc55
Syed NAzir Hussain <syednazir13@gmail.com>
2017-10-04 00:20:14 -0700

    update upload.php file

--- classes/upload.php
+++ classes/upload.php
@@ -2,8 +2,10 @@
 
 /*echo "<pre>";echo print_r($_FILES)."<br/>PatientID : ".$_POST['patientId'];exit;*/
 
+require_once 'pdocrudhandler.php';
+
 if(isset($_POST['patientId']) && isset($_FILES['files'])){
-    uploadMultipleFiles($_POST['patientId']);
+    uploadMultipleFiles($_POST['prescribtionId']);
 }
 
 function uploadMultipleFiles($Id){
@@ -21,32 +23,36 @@ function uploadMultipleFiles($Id){
                 $tmpFilePath = $_FILES['files']['tmp_name'][$i];
                 if ($tmpFilePath != ""){
                     $newFilePath = '../Project/uploads/'.$Id.'_'.$_FILES['files']['name'][$i];
+                    ($i == 0)? $path .= '../Project/uploads/'.$Id.'_'.$_FILES['files']['name'][$i] : $path .= '^../Project/uploads/'.$Id.'_'.$_FILES['files']['name'][$i];
                     if(move_uploaded_file($tmpFilePath,$newFilePath)) {
 
                     }
                 }
             }
         }
-        $path .= $Id.'_'.$_FILES['files']['name'][$i].' ';
         $files .= $_FILES['files']['name'][$i].' ';
     }
     $existfilesArray = explode(' ',$existfiles);
     $filesArray = explode(' ',$files);
-    if (count($existfilesArray) > 0 && $existfiles != ''){
-        $status .= '<div class="alert alert-success alert-dismissible fade in" role="alert">';
-        $status .= '<strong>Status: </strong>Files already shared <strong>'.$existfiles.'</strong> ';
-        $status .= '</div><br/><br/><br/>';
-        echo $status;
+    if (!empty($path)){
+        $_pdo = new pdocrudhandler();
+        $result = $_pdo->update('prescribtion',array('filePath' => $path),'where p_id = ?',array($Id));
+        if ($result['status'] == 'success' && $result['rowsAffected'] > 0){
+            if (count($existfilesArray) > 0 && $existfiles != ''){
+                $status .= '<div class="alert alert-success alert-dismissible fade in" role="alert">';
+                $status .= '<strong>Status: </strong>Files already shared <strong>'.$existfiles.'</strong> ';
+                $status .= '</div><br/><br/><br/>';
+                echo $status;
+            }
+            if (count($filesArray) > 0 && $files != ''){
+                $status .= '<div class="alert alert-success alert-dismissible fade in" role="alert">';
+                $status .= '<strong>Status: </strong>Files uploaded successfully <strong>'.$files.'</strong> ';
+                $status .= '</div><br/><br/>';
+                echo $status;
+            }
+        }
     }
-    if (count($filesArray) > 0 && $files != ''){
-        $status .= '<div class="alert alert-success alert-dismissible fade in" role="alert">';
-        $status .= '<strong>Status: </strong>Files uploaded successfully <strong>'.$files.'</strong> ';
-        $status .= '</div><br/><br/>';
-        echo $status;
-    }
-/*    $res = $_pdo->update('complaints',array('images'=>trim($path,"^")),'where id = ?', array($Id));
-    if($res['status'] = 'success' && $res['rowsAffected'] == 1){
-    }*/
+    /*$filePathArray = explode("^",$path);*/
 }
 
 
